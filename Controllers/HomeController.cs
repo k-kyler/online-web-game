c960a148d6315628aa0d4887df942d43aa14558e
k-kyler <khaiquang690@gmail.com>
2021-10-22 14:54:29 +0700

    Addd navbar item active styles, sign up and sign in route and UI

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace OnlineWebGame.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(() => {
+    // Configure styles for nav item active
+    let navItemActive = sessionStorage.getItem("nav-item-active");
+    
+    if (navItemActive) {
+        $(`#navbarNav .nav-item:nth-child(${navItemActive})`).addClass("active");
+    } else {
+        $(`#navbarNav .nav-item:nth-child(1)`).addClass("active");
+    }
+    
+    $(".navbar-brand").click(event => {
+        sessionStorage.setItem("nav-item-active", 1)
+    });
+    
+    $("#navbarNav .nav-item:nth-child(1)").click(event => {
+        sessionStorage.setItem("nav-item-active", 1)
+    });
+    
+    $("#navbarNav .nav-item:nth-child(2)").click(event => {
+        sessionStorage.setItem("nav-item-active", 2)
+    });
+    
+    $("#navbarNav .nav-item:nth-child(3)").click(event => {
+        sessionStorage.setItem("nav-item-active", 3)
+    });
+})

a514def4ac80d2e5f90cf6ffcdfea434d18228c7
k-kyler <khaiquang690@gmail.com>
2021-11-04 12:10:57 +0700

    Split css and js codes into separate files

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,39 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$(document).ready(() => {
-    // Configure styles for nav item active
-    let navItemActive = sessionStorage.getItem("nav-item-active");
-    
-    if (navItemActive) {
-        $(`#navbarNav .nav-item:nth-child(${navItemActive})`).addClass("active");
-    } else {
-        $(`#navbarNav .nav-item:nth-child(1)`).addClass("active");
-    }
-    
-    $(".navbar-brand").click(() => {
-        sessionStorage.setItem("nav-item-active", 1)
-    });
-    
-    $("#navbarNav .nav-item:nth-child(1)").click(() => {
-        sessionStorage.setItem("nav-item-active", 1)
-    });
-    
-    $("#navbarNav .nav-item:nth-child(2)").click(() => {
-        sessionStorage.setItem("nav-item-active", 2)
-    });
-    
-    $("#navbarNav .nav-item:nth-child(3)").click(() => {
-        sessionStorage.setItem("nav-item-active", 3)
-    });
-
-    $("#navbarNav .nav-item:nth-child(4)").click(() => {
-        sessionStorage.setItem("nav-item-active", 4)
-    });
-    
-    // Force setting modal to hide when open exit game modal
-    $("#exitGameConfirm").click(() => {
-        $("#myModal1").modal("hide")
-        $(".modal-backdrop").css("display", "none")
-    })
-})

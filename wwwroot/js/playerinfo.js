cbc1677af96c2625bbecaa40d069d7fe5a08f3a4
k-kyler <khaiquang690@gmail.com>
2021-12-13 20:09:42 +0700

    Add set correct avatar for player

--- wwwroot/js/playerinfo.js
+++ wwwroot/js/playerinfo.js
@@ -1,5 +1,16 @@
 $(document).ready(() => {
+  // Open player info modal
   $("#playerInfoModalTrigger").click(() => {
     $("#playerInfoModal").modal("show");
   });
 });
+
+// Set player avatar by gender
+const URLParams = new URLSearchParams(window.location.search);
+const { gd } = Object.fromEntries(URLParams.entries());
+
+if (gd === "male") {
+  document.getElementById("playerInfoAvatar").setAttribute("src", "assets/gentleman_character.png")
+} else {
+  document.getElementById("playerInfoAvatar").setAttribute("src", "assets/ladygirl_character.png")
+}

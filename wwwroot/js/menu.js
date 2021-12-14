659eb71df41ff396211d581659e5537fdaa606aa
k-kyler <khaiquang690@gmail.com>
2021-12-14 23:03:22 +0700

    Add settings and exit game modals to hall

--- wwwroot/js/menu.js
+++ wwwroot/js/menu.js
@@ -1,42 +1,41 @@
 $(document).ready(() => {
-    const DEV_URL = "https://localhost:5001";
-    const PRO_URL = "http://localhost:8000";
+  const DEV_URL = "https://localhost:5001";
+  const PRO_URL = "http://localhost:8000";
 
-    // Force setting modal to hide when open exit game modal
-    $("#exitGameConfirm").click(() => {
+  // Force setting modal to hide when open exit game modal
+  $("#exitGameConfirm").click(() => {
+    $("#myModal1").modal("hide");
+    $(".modal-backdrop").css("display", "none");
+  });
+
+  // Press to open Settings modal
+  $(document).keydown((e) => {
+    if (e.keyCode == 27) {
+      if ($("#myModal1").modal("hide")) {
+        $("#myModal1").modal("show");
+        $("#myModal2").modal("hide");
+        $(".modal-backdrop").css("display", "none");
+      } else if ($("#myModal1").modal("show")) {
         $("#myModal1").modal("hide");
         $(".modal-backdrop").css("display", "none");
-    });
+      }
+    }
+  });
 
-    // Press to open Settings modal
-    $(document).keydown((e) => {
-        if (e.keyCode == 27) {
-            if ($("#myModal1").modal("hide")) {
-                $("#myModal1").modal("show");
-                $("#myModal2").modal("hide");
-                $(".modal-backdrop").css("display", "none");
-            }
-            else if ($("#myModal1").modal("show")) {
-                $("#myModal1").modal("hide");
-                $(".modal-backdrop").css("display", "none");
-            }
-        }
+  // Sign out
+  $("#signOutButton").click(async () => {
+    const response = await fetch(`${DEV_URL}/signout`, {
+      method: "POST",
     });
-    
-    // Sign out
-    $("#signOutButton").click(async () => {
-        const response = await fetch(`${DEV_URL}/signout`, {
-            method: "POST"
-        })
-        const { code } = await response.json();
-                
-        if (code === "success") window.location.href = "/"
-    });
-    
-    // Press M to get back to menu page
-    window.addEventListener("keydown", event => {
-        if (event.key === "m") {
-            window.location.href = "/menu"
-        }
-    })
+    const { code } = await response.json();
+
+    if (code === "success") window.location.href = "/";
+  });
+
+  // Press M to get back to menu page
+  window.addEventListener("keydown", (event) => {
+    if (event.key === "m") {
+      window.location.href = "/menu";
+    }
+  });
 });
---
+++ wwwroot/js/settings.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+  $("#settingsButton").click(() => {
+    $("#myModal1").modal("show");
+  });
+});

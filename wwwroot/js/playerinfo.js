67ec22978f1e5a66e7ca24ac632db0b877eadf86
k-kyler <khaiquang690@gmail.com>
2021-12-15 21:50:43 +0700

    Add settings features

--- wwwroot/js/playerinfo.js
+++ wwwroot/js/playerinfo.js
@@ -4,13 +4,3 @@ $(document).ready(() => {
     $("#playerInfoModal").modal("show");
   });
 });
-
-// Set player avatar by gender
-const URLParams = new URLSearchParams(window.location.search);
-const { gd } = Object.fromEntries(URLParams.entries());
-
-if (gd === "male") {
-  document.getElementById("playerInfoAvatar").setAttribute("src", "assets/gentleman_character.png")
-} else {
-  document.getElementById("playerInfoAvatar").setAttribute("src", "assets/ladygirl_character.png")
-}
--- wwwroot/js/settings.js
+++ wwwroot/js/settings.js
@@ -1,5 +1,44 @@
 $(document).ready(() => {
+  // Open settings modal in hall
   $("#settingsButton").click(() => {
     $("#myModal1").modal("show");
+  });
+
+  // Volume and brightness features
+  let audioSource = document.getElementById("audioSource");
+  let volumeBar = document.getElementById("volumeBar");
+
+  audioSource.volume = 0.5;
+
+  if (!localStorage.getItem("audioVolume")) {
+    localStorage.setItem("audioVolume", audioSource.volume);
+  }
+  // else {
+  audioSource.volume = localStorage.getItem("audioVolume");
+  volumeBar.value = audioSource.volume * 100;
+  // }
+
+  $("#volumeBar").change((event) => {
+    audioSource.volume = event.target.value / 100;
+    localStorage.setItem("audioVolume", audioSource.volume);
+  });
+
+  // Brightness feature
+  let brightnessBar = document.getElementById("brightnessBar");
+  let brightnessValue = 1;
+
+  if (!localStorage.getItem("brightness")) {
+    localStorage.setItem("brightness", brightnessValue);
+  } else {
+    brightnessValue = localStorage.getItem("brightness");
+    document.documentElement.style.filter =
+      "brightness(" + brightnessValue + ")";
+    brightnessBar.value = brightnessValue * 100;
+  }
+
+  $("#brightnessBar").change((event) => {
+    document.documentElement.style.filter =
+      "brightness(" + event.target.value / 100 + ")";
+    localStorage.setItem("brightness", event.target.value / 100);
   });
 });

a6c126147fd2809d4edf504522140221811125e8
Kent-Khang <vinhkhang1969@gmail.com>
2022-01-07 12:40:53 +0700

    fix some

--- AutoUpdateStamina.cs
+++ AutoUpdateStamina.cs
@@ -25,41 +25,35 @@ namespace OnlineWebGame
             _logger = logger;
         }
 
+        // Timed Hosted Service running.
         public Task StartAsync(CancellationToken stoppingToken)
         {
-
             _context = _scopeFactory.CreateScope().ServiceProvider.GetRequiredService<GameOnlineContext>();
-
             _logger.LogInformation("Timed Hosted Service running.");
-
-            _timer = new Timer(DoWork, null, TimeSpan.Zero,
-                TimeSpan.FromSeconds(60));
-
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(3600));
             return Task.CompletedTask;
         }
 
+        // Timed Hosted Service is working.
         private void DoWork(object? state)
         {
             var count = Interlocked.Increment(ref executionCount);
-
-            _logger.LogInformation(
-                "Timed Hosted Service is working. Count: {Count}", count);
+            _logger.LogInformation("Timed Hosted Service is working. Count: {Count}", count);
             var userInfoDAO = new UserInfoDAO(_context);
+            // Get list player with low stamina (stamina < 100)
             var user = userInfoDAO.getLowStaPlayers();
-            user.ForEach(u =>
-            {
-                u.Stamina += 1;
-                
+            // +10 stamina per hour
+            user.ForEach(u => {
+                u.Stamina += 10;
                 userInfoDAO.updateUserSta(u);
             });
         }
 
+        // Timed Hosted Service is stopping.
         public Task StopAsync(CancellationToken stoppingToken)
         {
             _logger.LogInformation("Timed Hosted Service is stopping.");
-
             _timer?.Change(Timeout.Infinite, 0);
-
             return Task.CompletedTask;
         }
 
